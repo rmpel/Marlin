49fd480cf3a1d427faea6d863d95576a2bfe4163
Remon Pel <remon@clearsite.nl>
2021-02-13 12:54:27 +0100

    Disable stealthchop on extruder to enable linear advance

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2564,7 +2564,7 @@
    */
   #define STEALTHCHOP_XY
   #define STEALTHCHOP_Z
-  #define STEALTHCHOP_E
+  // #define STEALTHCHOP_E
 
   /**
    * Optimize spreadCycle chopper parameters by using predefined parameter sets
